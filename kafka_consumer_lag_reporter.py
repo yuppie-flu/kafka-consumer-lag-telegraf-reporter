b9d6cabaa46308d162766237c820fefc62e9f5e7
Kirill Kozlov <kozlov.k.e@gmail.com>
2017-12-21 18:12:01 +0100

    Update to Kafka 1.0 support, remove some fields from influx report

--- kafka_consumer_lag_reporter.py
+++ kafka_consumer_lag_reporter.py
@@ -1,11 +1,11 @@
+import os
 import subprocess
 import argparse
 
 # VERSION 0.1
 # from https://github.com/paksu/kafka-consumer-lag-telegraf-reporter
 
-OUTPUT_KEYS = ['group', 'topic', 'partition', 'current_offset', 'log_end_offset', 'lag']
-
+OUTPUT_KEYS = ['topic', 'partition', 'lag']
 
 def parse_output(input_from_checker):
     """
@@ -14,16 +14,15 @@ def parse_output(input_from_checker):
     """
     output = []
     for line in input_from_checker:
-        # Skip header
-        if 'GROUP, TOPIC, PARTITION' not in line:
-            columns = line.split(', ')
+        # Parse only lines with data
+        if line.strip() and not line.startswith('TOPIC'):
+            columns = line.split()
 
             # Only pick the columns we are interested in
-            group = columns[0]
             topic = columns[1]
-            metric_columns = [int(c) for c in columns[2:6]]
+            metric_columns = [int(c) for c in [columns[1],columns[4]]]
 
-            key_and_value_pairs = zip(OUTPUT_KEYS, [group, topic] + metric_columns)
+            key_and_value_pairs = zip(OUTPUT_KEYS, [topic] + metric_columns)
             output.append(dict(key_and_value_pairs))
 
     return output
@@ -33,7 +32,7 @@ def to_line_protocol(parsed_output):
     """
     Converts the parsed output to InfluxDB line protocol metrics
     """
-    return ["kafka.consumer_offset,topic={topic},group={group},partition={partition} current_offset={current_offset},log_end_offset={log_end_offset},lag={lag}"
+    return ["kafka.consumer_offset,topic={topic},partition={partition},lag={lag}"
             .format(**line) for line in parsed_output]
 
 
@@ -41,7 +40,7 @@ def get_kafka(args):
     """
     Gets consumer offsets from kafka via kafka-consumer-groups.sh
 
-    Should work on Kafka 0.8.0.2 and 0.9.0.1
+    Tested on Kafka 1.0
     """
     # append trailing slash
     if args.kafka_dir[-1] != "/":
@@ -52,12 +51,9 @@ def get_kafka(args):
         '--group {}'.format(args.group),
         '--describe'
     ]
-    if args.zookeeper:
-        params = params + ['--zookeeper', args.zookeeper]
-    else:
-        params = params + ['--new-consumer', '--bootstrap-server', args.bootstrap_server]
-
-    cmd = subprocess.Popen(" ".join(params), shell=True, stdout=subprocess.PIPE)
+    params = params + ['--bootstrap-server', args.bootstrap_server]
+    fnull = open(os.devnull, 'w')
+    cmd = subprocess.Popen(" ".join(params), shell=True, stdout=subprocess.PIPE, stderr=fnull)
 
     return [line for line in cmd.stdout]
 
@@ -65,10 +61,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--kafka-dir', default='/opt/kafka/', help='Kafka base directory', required=True)
     parser.add_argument('--group', help='Kafka group to check', required=True)
-    parser.add_argument('--bootstrap-server', help='Which kafka to query. Used for new consumer')
-    parser.add_argument('--zookeeper', help='Which zookeeper to query. Used for old consumer')
+    parser.add_argument('--bootstrap-server', help='Which kafka to query (for new consumers)', required=True)
     args = parser.parse_args()
-    assert args.zookeeper or args.bootstrap_server, "requires either --zookeeper or --bootstarp-server"
 
     output = get_kafka(args)
     parsed_output = parse_output(output)
